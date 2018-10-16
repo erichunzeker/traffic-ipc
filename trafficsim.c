34d557de75a0ee7ae1b6edacea3f1e9d3691b3a1
Eric Hunzeker <emh128.pitt.edu>
2018-10-16 10:03:53 -0400

    start working on main

--- trafficsim.c
+++ trafficsim.c
@@ -1,19 +1,32 @@
-#include "unistd.h"
+#include <linux/unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
-#define __NR_cs1550_down 326;
-#define __NR_cs1550_up 325;
+#define __NR_cs1550_down 326
+#define __NR_cs1550_up 325
 
-struct cs1550_sem
-{
-    int value;
-    //Some queue of your devising
+struct cs1550_node {
+    struct cs1550_node* next;
+    struct task_struct* task;
 };
 
-void down(cs1550_sem *sem) {
+struct cs1550_sem {
+    int value;
+    struct cs1550_node* front;
+    struct cs1550_node* back;
+};
+
+void down(struct cs1550_sem *sem) {
     syscall(__NR_cs1550_down, sem);
 }
-
-void up(cs1550_sem *sem) {
+void up(struct cs1550_sem *sem) {
     syscall(__NR_cs1550_up, sem);
+}
+
+
+int main() {
+    printf("test");
+    return 0;
 }
 
