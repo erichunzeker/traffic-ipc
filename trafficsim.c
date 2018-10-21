26b56f3cf888b21b4e681df1b5cf70f46aee893c
Eric Hunzeker <emh128.pitt.edu>
2018-10-21 19:40:27 -0400

    added times

--- trafficsim.c
+++ trafficsim.c
@@ -2,192 +2,199 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <time.h>
 
 #define __NR_cs1550_down 326
 #define __NR_cs1550_up 325
 
 struct cs1550_sem {
     int value;
-    struct cs1550_node* front;
-    struct cs1550_node* back;
+    struct Node *front;
+    struct Node *back;
+};
+
+struct semQueues {
+    int north[10];
+    int south[10];
+
+    int northFront;
+    int southFront;
+    int northBack;
+    int southBack;
+
+    int northCount;
+    int southCount;
+
+    struct cs1550_sem northFull;
+    struct cs1550_sem southFull;
+
+    struct cs1550_sem northEmpty;
+    struct cs1550_sem southEmpty;
+
+    struct cs1550_sem northSem;
+    struct cs1550_sem southSem;
+
+    struct cs1550_sem NSFull;
 };
 
 void down(struct cs1550_sem *sem) {
     syscall(__NR_cs1550_down, sem);
 }
+
 void up(struct cs1550_sem *sem) {
     syscall(__NR_cs1550_up, sem);
 }
 
-int north[10];
-int south[10];
-
-int *northFront;
-int *northBack;
-
-int *southFront;
-int *southBack;
-
-
-
-int *northCount;
-int *southCount;
-
-struct cs1550_sem northSem;
-struct cs1550_sem southSem;
-
-struct cs1550_sem northSemEmpty;
-struct cs1550_sem southSemEmpty;
-
-struct cs1550_sem northSemFull;
-struct cs1550_sem southSemFull;
-
-struct cs1550_sem NSFull;
-
-
-void producerNorth();
-void producerSouth();
-void flagMan();
-
-int main() {
-    *northCount = 0;
-    *southCount = 0;
-    *northFront = 0;
-    *northBack = 0;
-    *southFront = 0;
-    *southBack = 0;
-
-    void *ptr = mmap(NULL, (sizeof(north) + sizeof(south) ), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
-
-    northFront = ptr;
-
-    northSem.value = 1;
-    southSem.value = 1;
-    northSem.front = NULL;
-    southSem.front = NULL;
-    northSem.back = NULL;
-    southSem.back = NULL;
-
-    northSemEmpty.value = 10;
-    southSemEmpty.value = 10;
-    northSemEmpty.front = NULL;
-    southSemEmpty.front = NULL;
-    northSemEmpty.back = NULL;
-    southSemEmpty.back = NULL;
-
-    northSemFull.value = 0;
-    southSemFull.value = 0;
-    northSemFull.front = NULL;
-    southSemFull.front = NULL;
-    northSemFull.back = NULL;
-    southSemFull.back = NULL;
-
-    NSFull.value = 0;
-    NSFull.front = NULL;
-    NSFull.back = NULL;
-
-    printf("TRAFFICSIM - eric hunzeker\n\n");
-
-    if(fork() == 0) {
-        producerNorth();
-    } if(fork() == 0) {
-        producerSouth();
-    } if(fork() == 0) {
-        flagMan();
-    }
-
-    wait(NULL);
-    return 0;
-}
+struct semQueues *mem;
 
 void producerNorth() {
-    while(1) {
-        down(&northSemEmpty);
-        down(&northSem);
-        *(north + *northBack) = *northBack;
-        *northBack = (*northBack + 1) % 10;
-        printf("Car %d coming from the %c direction arrived in the queue at time %d.\n", *northCount);
-        if(NSFull.value == 0) {
-            printf("Car %d coming from the %c direction, blew their horn at time %d.", *northCount);
-        }
-        *northCount++;
-        up(&northSem);
-        up(&northSemFull);
-        up(&NSFull);
+    int n = 1;
+    while (1) {
+        down(&((*mem).northEmpty));
+        down(&((*mem).northSem));
+        *((*mem).north + (*mem).northBack) = (*mem).northBack;
+        (*mem).northBack = ((*mem).northBack + 1) % 10;
 
-        if(rand() % 10 > 8)
+        //https://stackoverflow.com/questions/5141960/get-the-current-time-in-c
+
+        time_t rawtime;
+        struct tm * timeinfo;
+
+        time ( &rawtime );
+        timeinfo = localtime ( &rawtime );
+
+        printf("Car %d coming from the North direction arrived in the queue at time %s .\n", n++, asctime (timeinfo));
+        (*mem).northCount++;
+        up(&((*mem).northSem));
+        up(&((*mem).northFull));
+        up(&((*mem).NSFull));
+        if ((rand()) % 10 > 8) {
             sleep(20);
+        }
     }
-};
+}
 
 void producerSouth() {
-    while(1) {
-        down(&southSemEmpty);
-        down(&southSem);
-        *(south + *southBack) = *southBack;
-        *southBack = (*southBack + 1) % 10;
-        printf("Car %d coming from the %c direction arrived in the queue at time %d.\n", *southCount);
-        if(NSFull.value == 0) {
-            printf("Car %d coming from the %c direction, blew their horn at time %d.", *southCount);
-        }
-        *southCount++;
-        up(&southSem);
-        up(&southSemFull);
-        up(&NSFull);
-
-        if(rand() % 10 > 8)
-            sleep(20);
-    }
-};
-
-void flagMan() {
-    *northCount = 0;
-    *southCount = 0;
+    int n = 1;
     while (1) {
-        if (*northCount == 0 && *southCount == 0) {
-            printf("The flagperson is now asleep.\n");
-            down(&NSFull);
+        down(&((*mem).southEmpty));
+        down(&((*mem).southSem));
+        *((*mem).south + (*mem).southBack) = (*mem).southBack;
+        (*mem).southBack = ((*mem).southBack + 1) % 10;
 
-            if(*northCount > 0) {
-                printf("Car %d coming from the %c direction, blew their horn at time %d.");
-            }
+        time_t rawtime;
+        struct tm * timeinfo;
 
-            up(&NSFull);
+        time ( &rawtime );
+        timeinfo = localtime ( &rawtime );
 
-        }
-
-
-
-        while(*northCount > 0) {
-            down(&northSemFull);
-            down(&northSem);
-            *northFront = (*northFront + 1) % 10;
-            printf("Car %d coming from the %c direction left the construction zone at time %d.", *northFront);
-            *northCount--;
-            up(&northSem);
-            up(&northSemEmpty);
-            down(&NSFull);
-            sleep(2);
-
-            if(*southCount == 10) {
-                break;
-            }
-        }
-
-        while(*southCount > 0) {
-            down(&southSemFull);
-            down(&southSem);
-            *southFront = (*southFront + 1) % 10;
-            printf("Car %d coming from the %c direction left the construction zone at time %d.", *southFront);
-            *southCount--;
-            up(&southSem);
-            up(&southSemEmpty);
-            down(&NSFull);
-            sleep(2);
-
-            if(*northCount == 10) {
-                break;
-            }
+        printf("Car %d coming from the South direction arrived in the queue at time %s .\n", n++, asctime (timeinfo));
+        (*mem).southCount++;
+        up(&((*mem).southSem));
+        up(&((*mem).southFull));
+        up(&((*mem).NSFull));
+        if ((rand()) % 10 > 8) {
+            sleep(20);
         }
     }
+}
+
+void flagman() {
+    int northCar = 1;
+    int southCar = 1;
+    while (1) {
+        if (((*mem).northCount == 1) && ((*mem).southCount == 1)) {
+            printf("\nThe flagperson is now asleep.\n");
+            down(&((*mem).NSFull));
+            time_t rawtime;
+            struct tm * timeinfo;
+            time ( &rawtime );
+            timeinfo = localtime ( &rawtime );
+            if ((*mem).northCount > 1) {
+                printf("\nCar %d coming from the North direction, blew their horn at time %s .\n", northCar, asctime (timeinfo));
+            }
+            else if ((*mem).southCount > 1) {
+                printf("\nCar %d coming from the South direction, blew their horn at time %s .\n", southCar, asctime (timeinfo));
+            }
+            up(&((*mem).NSFull));
+        }
+
+        while ((*mem).northCount > 1 && (*mem).southCount == 11) {
+            down(&((*mem).northFull));
+            down(&((*mem).northSem));
+            (*mem).northFront = ((*mem).northFront + 1) % 10;
+            time_t rawtime;
+            struct tm * timeinfo;
+            time ( &rawtime );
+            timeinfo = localtime ( &rawtime );
+            printf("Car %d coming from the North direction left the construction zone at time %s .\n", northCar++, asctime (timeinfo));
+            (*mem).northCount--;
+            up(&((*mem).northSem));
+            up(&((*mem).northEmpty));
+            down(&((*mem).NSFull));
+            sleep(2);
+        }
+
+        while ((*mem).southCount > 1 && (*mem).northCount < 11) {
+            down(&((*mem).southFull));
+            down(&((*mem).southSem));
+            (*mem).southFront = ((*mem).southFront + 1) % 10;
+
+            time_t rawtime;
+            struct tm * timeinfo;
+
+            time ( &rawtime );
+            timeinfo = localtime ( &rawtime );
+
+            printf("Car %d coming from the South direction left the construction zone at time %s.\n", southCar++, asctime (timeinfo));
+            (*mem).southCount--;
+            up(&((*mem).southSem));
+            up(&((*mem).southEmpty));
+            down(&((*mem).NSFull));
+            sleep(2);
+        }
+    }
+}
+
+int main() {
+    mem = mmap(NULL, sizeof(struct semQueues), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+
+    (*mem).northCount = 1;
+    (*mem).northBack = 0;
+    (*mem).southCount = 1;
+    (*mem).southBack = 0;
+
+    (*mem).northEmpty.value = 10;
+    (*mem).northEmpty.front = NULL;
+    (*mem).northEmpty.back = NULL;
+    (*mem).southEmpty.value = 10;
+    (*mem).southEmpty.front = NULL;
+    (*mem).southEmpty.back = NULL;
+
+    (*mem).northFull.value = 0;
+    (*mem).northFull.front = NULL;
+    (*mem).northFull.back = NULL;
+    (*mem).southFull.value = 0;
+    (*mem).southFull.front = NULL;
+    (*mem).southFull.back = NULL;
+
+    (*mem).northSem.value = 1;
+    (*mem).northSem.front = NULL;
+    (*mem).northSem.back = NULL;
+    (*mem).southSem.value = 1;
+    (*mem).southSem.front = NULL;
+    (*mem).southSem.back = NULL;
+
+    (*mem).NSFull.value = 0;
+    (*mem).NSFull.front = NULL;
+    (*mem).NSFull.back = NULL;
+
+    if (fork() == 0) {
+        flagman();
+    } if (fork() == 0) {
+        producerNorth();
+    } if (fork() == 0) {
+        producerSouth();
+    }
+    wait(NULL);
 }
